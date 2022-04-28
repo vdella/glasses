9c759e33af72e18f894f77f50a10962836129c81
vdella <vitor.origamer@gmail.com>
2022-04-28 13:07:19 -0300

    added ref to last element added to set

--- structures/holder.py
+++ structures/holder.py
@@ -1,12 +1,12 @@
 from dataclasses import dataclass
+from structures.simple import Point
 
 
 @dataclass
 class PointHolder:
     points = set()
+    last: Point = Point(0, 0)
 
     def push(self, point):
-        return self.points.add(point)
-
-    def pop(self):
-        return self.points.pop()
+        self.last = point
+        return self.points.add(self.last)
