81b4adb9a7d2981a3944cda0f384db167865df78
vdella <vitor.origamer@gmail.com>
2022-04-28 10:24:46 -0300

    deleted ui/viewport
    	* Added its function of viewport transformation to
    	the operations module.

--- ui/viewport.py
+++
@@ -1,8 +0,0 @@
-def transform(point, window, viewport):
-    # Keeps the X-axis...
-    x_viewport = ((point.x - window.x.min) / (window.x.max - window.x.min)) * (viewport.x.max - viewport.x.min)
-
-    # But inverts the Y's.
-    y_viewport = (1 - (point.y - window.y.min) / (window.y.max - window.y.min)) * (viewport.y.max - viewport.y.min)
-
-    return x_viewport, y_viewport
