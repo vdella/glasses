4323b185069d0eaf6377980f4987b0db5f164f88
vdella <vitor.origamer@gmail.com>
2022-04-28 10:55:00 -0300

    created constants module

---
+++ constants.py
@@ -0,0 +1,14 @@
+class Window:  # As the root's dimensions.
+    X_MIN = 0
+    X_MAX = 750
+
+    Y_MIN = 0
+    Y_MAX = 500
+
+
+class Viewport:  # As the root's Canvas dimensions.
+    X_MIN = 0
+    X_MAX = Window.X_MAX / 2
+
+    Y_MIN = 0
+    Y_MAX = Window.Y_MAX / 2
