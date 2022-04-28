87a2d3dd2a693add8767db43a8915509d167aafb
vdella <vitor.origamer@gmail.com>
2022-04-28 11:31:31 -0300

    simplified readability with use of constants module

--- operations.py
+++ operations.py
@@ -1,29 +1,12 @@
 from structures.simple import Point
-
-
-class Constants:
-    class Window:  # As the root's dimensions.
-        X_MIN = 0
-        X_MAX = 750
-
-        Y_MIN = 0
-        Y_MAX = 500
-
-    class Viewport:  # As the root's Canvas dimensions.
-        X_MIN = 0
-        X_MAX = 750 / 2
-
-        Y_MIN = 0
-        Y_MAX = 500 / 2
+from constants import Window, Viewport
 
 
 def viewport_transformation(point):
     # Keeps the X-axis...
-    x_viewport = ((point.x - Constants.Window.X_MIN) / (Constants.Window.X_MAX - Constants.Window.X_MIN)) \
-        * (Constants.Viewport.X_MAX - Constants.Viewport.X_MIN)
+    x_viewport = ((point.x - Window.X_MIN) / (Window.X_MAX - Window.X_MIN)) * (Viewport.X_MAX - Viewport.X_MIN)
 
     # But inverts the Y's.
-    y_viewport = (1 - (point.y - Constants.Window.Y_MIN) / (Constants.Window.Y_MAX - Constants.Window.Y_MIN)) \
-        * (Constants.Viewport.Y_MAX - Constants.Viewport.Y_MIN)
+    y_viewport = (1 - (point.y - Window.Y_MIN) / (Window.Y_MAX - Window.Y_MIN)) * (Viewport.Y_MAX - Viewport.Y_MIN)
 
     return Point(x_viewport, y_viewport)
