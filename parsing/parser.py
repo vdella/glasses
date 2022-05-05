cb53e9cfa606117048c8d47bb3ff1eed8e3b5a52
vdella <vitor.origamer@gmail.com>
2022-05-05 03:14:17 -0300

    created parsing for user input

--- parsing/parser.py
+++ parsing/parser.py
@@ -1,36 +1,76 @@
-from tkinter import *
+from structures.globals import point_cache
+from ui.error_presentation import ErrorBox
 
 
-class PointScanner:
+class Parser:
 
-    def __init__(self, mw):
-        self.prompt = Toplevel(mw.root)
-        self.prompt.title("Point prompt")
-        self.prompt.geometry("200x200")
+    cache = list()
 
-    def point_label_for(self, is_x_axis: bool):
-        axis = 'X' if is_x_axis else 'Y'
-        Label(self.prompt, text="Input {} for new point".format(axis)).pack()
+    def parse(self) -> dict:
+        """Reads user input. 'add' being the operation, generates a polygon
+        graph from the desired coordinates. If not, removes the said
+        coordinates from cache."""
+        self.digest_entry()
 
-    def parse(self) -> Entry:
-        entry = Entry(self.prompt)
-        entry.pack()
-        return entry
+        for line in self.cache:
+            operation = line[0]
+            operands: tuple = tuple([_format(x) for x in line[1:]])
 
-    def enter_button(self):
-        button_pressed = StringVar()
-        enter = Button(self.prompt, text='Enter', command=lambda: button_pressed.set('button pressed'))
-        enter.pack()
-        enter.wait_variable(button_pressed)
-        return enter
+            edges: dict = graph_from(operands)
 
-    def close(self):
-        self.prompt.destroy()
+            if operation == 'add':
+                _add(operands)
+                return edges
+            elif operation == 'rm' or operation == 'remove':
+                _remove(operands)
+                return edges
+            else:
+                ErrorBox()
+                return {}
+
+    def digest_entry(self):
+        """Digests a line of user input into
+        individual parts, separated by blank spaces."""
+
+        for i in range(len(self.cache)):
+            cache_entry = self.cache[i].split()
+            self.cache[i] = cache_entry
+        return self.cache
 
 
-class LineScanner(PointScanner):
-    def __init__(self, mw):
-        super().__init__(mw)
-        self.prompt = Toplevel(mw.root)
-        self.prompt.title("Line prompt")
-        self.prompt.geometry("200x400")
+def _format(written_point: str) -> tuple:
+    """Formats a string cartesian coordinate into a
+    point tuple."""
+    x_begin_index = written_point.index('(') + 1
+    x_end_index = written_point.index(',')
+
+    y_begin_index = written_point.index(',') + 1
+    y_end_index = written_point.index(')')
+
+    x, y = float(written_point[x_begin_index: x_end_index]), float(written_point[y_begin_index: y_end_index])
+
+    return x, y
+
+
+def graph_from(points: tuple):
+    """Generates a polygon-graph from a tuple of point coordinates.
+    Each point is directed to its successor; the last points
+    to the first."""
+    graph = dict()
+
+    first = points[0]
+    last = points[-1]
+    graph[last] = first
+
+    for i in range(len(points) - 1):
+        graph[points[i]] = points[i + 1]
+
+    return graph
+
+
+def _add(point):
+    point_cache.add(point)
+
+
+def _remove(point):
+    point_cache.remove(point)
