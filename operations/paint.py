8bddf00410e06d7a1271c9e545e9a9afb37f5555
vdella <vitor.origamer@gmail.com>
2022-05-06 00:02:19 -0300

    thinned user lines width

--- operations/paint.py
+++ operations/paint.py
@@ -5,4 +5,4 @@ def paint(x1, y1, x2, y2, canvas: Canvas) -> ():
     """Paints the outlined extremes of a line and the line itself."""
     canvas.create_oval(x1, y1, x1, y1, fill='black', outline='black', width=5)
     canvas.create_oval(x2, y2, x2, y2, fill='black', outline='black', width=5)
-    canvas.create_line(x1, y1, x2, y2, fill='black', width=3)
+    canvas.create_line(x1, y1, x2, y2, fill='black')
