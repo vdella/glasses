529fda3b8438017bae3923deb7398bceaad7cbee
vdella <vitor.origamer@gmail.com>
2022-05-06 00:03:18 -0300

    resized canvas

--- ui/canvas_frame.py
+++ ui/canvas_frame.py
@@ -3,6 +3,6 @@ import tkinter as tk
 
 class CanvasFrame:
 
-    def __init__(self, mainframe: tk.Frame, width, height):
-        self.canvas = tk.Canvas(mainframe, width=0.60*width, height=height, bg='white')
+    def __init__(self, mainframe: tk.Frame):
+        self.canvas = tk.Canvas(mainframe, bg='white')
         self.canvas.pack(side=tk.RIGHT, fill=tk.BOTH, expand=True)
