ca256f47d7628e9e6d9de18aa3e2d6e95fcd9b77
vdella <vitor.origamer@gmail.com>
2022-05-06 00:01:12 -0300

    renamed constants

--- operations/geometry.py
+++ operations/geometry.py
@@ -1,13 +1,13 @@
-from constants import Window, Viewport
+from constants import Window, Canvas
 
 
 def viewport_transformation(point: tuple):
     # Keeps the X-axis...
     x, y = point[0], point[1]
 
-    x_viewport = ((x - Window.X_MIN) / (Window.X_MAX - Window.X_MIN)) * (Viewport.X_MAX - Viewport.X_MIN)
+    x_viewport = ((x - Window.X_MIN) / (Window.X_MAX - Window.X_MIN)) * (Canvas.X_MAX - Canvas.X_MIN)
 
     # But inverts the Y's.
-    y_viewport = (1 - (y - Window.Y_MIN) / (Window.Y_MAX - Window.Y_MIN)) * (Viewport.Y_MAX - Viewport.Y_MIN)
+    y_viewport = (1 - (y - Window.Y_MIN) / (Window.Y_MAX - Window.Y_MIN)) * (Canvas.Y_MAX - Canvas.Y_MIN)
 
     return x_viewport, y_viewport
