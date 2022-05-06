bbb375e6b808394543359063386bb67c00d51409
vdella <vitor.origamer@gmail.com>
2022-05-05 23:59:43 -0300

    draws axis x and y

--- ui/paint_interface.py
+++ ui/paint_interface.py
@@ -7,3 +7,15 @@ def paint_graph(graph: dict, canvas):
         x1, y1 = viewport_transformation(key)
         x2, y2 = viewport_transformation(value)
         paint(x1, y1, x2, y2, canvas)
+
+
+def viewport_x(canvas):
+    x1, y1 = viewport_transformation((0, -3000))
+    x2, y2 = viewport_transformation((0, 3000))
+    canvas.create_line(x1, y1, x2, y2, fill='blue', width=5, dash=True)
+
+
+def viewport_y(canvas):
+    x1, y1 = viewport_transformation((-3000, 0))
+    x2, y2 = viewport_transformation((3000, 0))
+    canvas.create_line(x1, y1, x2, y2, fill='orange', width=5, dash=True, dashoffset=5)
