8c8ce28f2b7862eddb7a702bcb496bc4fa332149
vdella <vitor.origamer@gmail.com>
2022-05-19 16:21:07 -0300

    changed parse()
    	* parse() doesn't return a list no more. It just
    	maps the entries for a cache_interface function;
    	* digest() was changed into a protected static method
    	and the cache attribute of Parsing was removed.

--- parsing/parser.py
+++ parsing/parser.py
@@ -1,37 +1,29 @@
 from parsing.cache_interface import module_functions
-from structures.geometric_structures import Structure
 
 
 class Parser:
 
-    cache = list()
-
-    def parse(self) -> list:
+    def parse(self, entries) -> ():
         """Digests user input and draws a graph according
         to given coordinates."""
 
-        self.digest_entry()
+        digest_cached = self._digest(entries)
 
-        results = list()
-
-        for line in self.cache:
+        for line in digest_cached:
             operation = line[0]
             operands: tuple = tuple([_format(x) for x in line[1:]])
 
-            module_functions[operation](Structure(operands))
+            module_functions[operation](operands)
 
-            edges: dict = graph_from(operands)
-            results.append(edges)
-        return results
-
-    def digest_entry(self):
+    @staticmethod
+    def _digest(entries):
         """Digests a line of user input into
         individual parts, separated by blank spaces."""
 
-        for i in range(len(self.cache)):
-            cache_entry = self.cache[i].split()
-            self.cache[i] = cache_entry
-        return self.cache
+        for i in range(len(entries)):
+            cache_entry = entries[i].split()
+            entries[i] = cache_entry
+        return entries
 
 
 def _format(written_point: str) -> tuple:
@@ -46,19 +38,3 @@ def _format(written_point: str) -> tuple:
     x, y = float(written_point[x_begin_index: x_end_index]), float(written_point[y_begin_index: y_end_index])
 
     return x, y
-
-
-def graph_from(points: tuple):
-    """Generates a polygon-graph from a tuple of point coordinates.
-    Each point is directed to its successor; the last points
-    to the first."""
-    graph = dict()
-
-    first = points[0]
-    last = points[-1]
-    graph[last] = first
-
-    for i in range(len(points) - 1):
-        graph[points[i]] = points[i + 1]
-
-    return graph
