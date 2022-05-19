43671460700dc2782c824ea0128d91044547d357
vdella <vitor.origamer@gmail.com>
2022-05-19 16:16:03 -0300

    added __eq__, __hash__ and __len__ operators
    	* In order to use a Structure object inside a set, the structure
    	needs to be hashable. That can be accomplished by using
    	the hash of its points tuple;
    	* __eq__ is used implicitly to verify if a given tuple
    	is already inside the structure_cache set; if the
    	hashes of 2 tuples are the same, then they are the same tuple;
    	* __len__ was implemented to be used in __str__;
    	we need length checking for an instance in order to
    	know if its a point, line or polygon.

--- structures/geometric_structures.py
+++ structures/geometric_structures.py
@@ -1,18 +1,29 @@
 class Structure:
 
-    def __init__(self, points, label=''):
-        self.points = points
-        self.label = label
+    def __init__(self, points):
+        self.points: tuple = points
 
     def __str__(self):
         written = str(self.points)
-        if len(self.points) == 1:
+        if len(self) == 1:
             struct_type = 'Point'
             written = written.replace(',)', ')')
         elif len(self.points) == 2:
             struct_type = 'Line'
         else:
             struct_type = 'Polygon'
-        return '{} : {}'.format(struct_type, written) \
-            if self.label == '' \
-            else '{} : {} : {}'.format(self.label, struct_type, written)
+        return '{} : {}'.format(struct_type, written)
+
+    def __eq__(self, other):
+        if isinstance(other, Structure):
+            return True if self.points == other.points else False
+
+    def __hash__(self):
+        return hash(self.points)
+
+    def __len__(self):
+        return len(self.points)
+
+
+if __name__ == '__main__':
+    print(Structure(((0, 0),),))
